263e7179e3945aeaf92be5438f8b4eade7e7f87e
Joen Ho <ohneoj@outlook.com>
2020-01-25 18:16:04 -0800

    configure the app to work with databases, redirect the urls to the app

--- PythonClub/settings.py
+++ PythonClub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ClubApp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'PythonClub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pythonclub',
+        'USER': 'postgres',
+        'PASSWORD': 'ohneoj0128',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
