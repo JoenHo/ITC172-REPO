8528e90accab81ddf5fb40c20de331c898e3e595
Joen Ho <ohneoj@outlook.com>
2020-02-05 15:00:56 -0800

    create and register model class

--- ClubApp/models.py
+++ ClubApp/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Meeting(models.Model):
+    meetingtitle = models.CharField(max_length=255)
+    meetingdate = models.DateField()
+    meetingtime = models.TimeField()
+    meetinglocation = models.CharField(max_length=255, null=True, blank=True)
+    meetingagenda = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return super().__str__()
+
+    class Meta:
+        db_table='meeting'
+        verbose_name_plural='meetings'
+
+class MeetingMinutes(models.Model):
+    meetingid = models.CharField(max_length=255)
+    meetingattendance = models.ManyToManyField(User)
+    meetingminutes = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return super().__str__()
+
+    class Meta:
+        db_table='meetingMinutes'
+        verbose_name_plural='meetingsMinutes'
+
+class Resource(models.Model):
+    resourcename = models.CharField(max_length=255)
+    resourcetype = models.CharField(max_length=255, null=True, blank=True)
+    resourceurl = models.CharField(max_length=255, null=True, blank=True)
+    recourcedateentered = models.DateField()
+    resourceuserid = models.ForeignKey(User, on_delete=models.CASCADE)
+    resourcedescription = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return super().__str__()
+
+    class Meta:
+        db_table='resource'
+        verbose_name_plural='resource'
+
+class Event(models.Model):
+    eventtitle = models.CharField(max_length=255)
+    eventlocation = models.CharField(max_length=255, null=True, blank=True)
+    eventdate = models.DateField()
+    eventtime = models.TimeField()
+    eventdescription = models.CharField(max_length=255, null=True, blank=True)
+    eventuser = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return super().__str__()
+
+    class Meta:
+        db_table='event'
+        verbose_name_plural='event'
